cdb6181e182f25a943d042c2adcd536a4370e98e
Alexandre Devilliers <elbereth@dashninja.pl>
2015-09-10 15:09:04 +0200

    Fixed the license header in all files.

--- dashninja-blocks.js
+++ dashninja-blocks.js
@@ -1,4 +1,23 @@
-// DASH Masternode Ninja - Front-End - Blocks (v2)
+/*
+ This file is part of Dash Ninja.
+ https://github.com/elbereth/dashninja-fe
+
+ Dash Ninja is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Dash Ninja is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Dash Ninja.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+// Dash Ninja Front-End (dashninja-fe) - Blocks (v2)
 // By elberethzone / https://dashtalk.org/members/elbereth.175/
 
 var dashninjaversion = '2.2.1';
--- dashninja-config.js
+++ dashninja-config.js
@@ -1,3 +1,22 @@
+/*
+ This file is part of Dash Ninja.
+ https://github.com/elbereth/dashninja-fe
+
+ Dash Ninja is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Dash Ninja is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Dash Ninja.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 // Either indicate if we are we on testnet (=1) or on mainnet (=0)
 //var dashninjatestnet = 0;
 // OR indicate the hostname for testnet (if the hostname the page is running is equal to this, it will switch to testnet)
